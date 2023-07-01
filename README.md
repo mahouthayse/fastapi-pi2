a2d10ba8f5a8b13f4568bd7b7d110b28893479e9
opkspaty <mcpedrohenriquelc@gmail.com>
2023-07-01 14:11:40 -0300

    feat: users table

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fastapi-pi2
 Projeto final de Programação para Internet 2 utilizando fastAPI e SQLAlchemy.
+ 
--- main.py
+++ main.py
@@ -1,29 +1,45 @@
+import uuid
+
 from fastapi import FastAPI, status
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, CHAR
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("sqlite://todo.db")
-Base = declarative_base()
-Base.metadata.create_all(engine)
+
+
+#engine = create_engine("sqlite://user.db")
+#Base = declarative_base()
+#Base.metadata.create_all(engine)
 app = FastAPI()
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, default=str(uuid.uuid4()))
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    gender = Column(CHAR, unique=True, index=True)
+    password_hash = Column(String)
+
 
 @app.get("/")
 def root():
     return "root"
-@app.post("/todo", status_code=201)
-def create_todo():
-    return "create todo item"
+@app.post("/user", status_code=201)
+def create_user():
+    return "create user item"
 
-@app.get("/todo/{id}")
-def read_todo(id: int):
-    return "read todo item with id {id}"
-@app.put("/todo/{id}")
-def update_todo(id: int):
-    return "update todo item"
-@app.delete("/todo/{id}", status_code=status.HTTP_200_OK)
-def delete_todo(id: int):
-    return "delete todo item"
-@app.get("/todo")
-def read_todo_list():
-    return "read todo list"
+@app.get("/user/{id}")
+def read_user(id: int):
+    return "read user item with id {id}"
+@app.put("/user/{id}")
+def update_user(id: int):
+    return "update user item"
+@app.delete("/user/{id}", status_code=status.HTTP_200_OK)
+def delete_user(id: int):
+    return "delete user item"
+@app.get("/user")
+def read_user_list():
+    return "read user list"
 
