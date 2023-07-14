e5edd7166674ac260b223dcf7fa70b8bdc20ab7a
mahouThayse <dasilva.thaysef@gmail.com>
2023-07-14 11:31:47 -0300

    Modificações no basemodel do método create e update

--- main.py
+++ main.py
@@ -7,11 +7,25 @@ import uuid
 
 from fastapi import FastAPI, status, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8080",
+]
 engine = create_engine('postgresql://postgres:root@localhost/PI2CRUD')
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class User(Base):
     __tablename__ = "users"
@@ -32,34 +46,46 @@ class User(Base):
 
 Base.metadata.create_all(bind=engine)
 
+class UserCreateUpdateModel(BaseModel):
+    name: str
+    age: int
+    cpf: str
+    gender: str
+    email: str
+    zipcode: str
+    address: str
+    address_number: str
+    address_neighborhood: str
+    address_city: str
+    address_state: str
+    password: str
+
 
 @app.get("/")
 def root():
     return "root"
 
 @app.post("/users")
-def create_user(name: str, age: int, cpf: str, gender: str, email: str, zipcode: str,
-                address: str, address_number: str, address_neighborhood: str, address_city: str,
-                address_state: str, password: str):
+def create_user(user_data: UserCreateUpdateModel):
     db = SessionLocal()
     db_user = User(
-        name=name,
-        age=age,
-        cpf=cpf,
-        gender=gender,
-        email=email,
-        zipcode=zipcode,
-        address=address,
-        address_number=address_number,
-        address_neighborhood=address_neighborhood,
-        address_city=address_city,
-        address_state=address_state,
-        password=password
+        name=user_data.name,
+        age=user_data.age,
+        cpf=user_data.cpf,
+        gender=user_data.gender,
+        email=user_data.email,
+        zipcode=user_data.zipcode,
+        address=user_data.address,
+        address_number=user_data.address_number,
+        address_neighborhood=user_data.address_neighborhood,
+        address_city=user_data.address_city,
+        address_state=user_data.address_state,
+        password=user_data.password
     )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return JSONResponse(content={"message": "Usuário Criado com sucesso"})
+    return JSONResponse(content={"message": "Usuário criado com sucesso"})
 
 @app.get("/users/{id}")
 def get_user(id: str):
@@ -83,9 +109,30 @@ def get_user(id: str):
     }
     return JSONResponse(content=user_dict)
 
-@app.put("/user/{id}")
-def update_user(id: int):
-    return "update user item"
+@app.patch("/users/{id}")
+def update_user(id: str, user_data: UserCreateUpdateModel):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user.name = user_data.name
+    user.age = user_data.age
+    user.cpf = user_data.cpf
+    user.gender = user_data.gender
+    user.email = user_data.email
+    user.zipcode = user_data.zipcode
+    user.address = user_data.address
+    user.address_number = user_data.address_number
+    user.address_neighborhood = user_data.address_neighborhood
+    user.address_city = user_data.address_city
+    user.address_state = user_data.address_state
+    user.password = user_data.password
+
+    db.commit()
+    db.refresh(user)
+    return JSONResponse(content={"message": "Usuário Atualizado com sucesso"})
+
 @app.delete("/users/{id}")
 def delete_user(id: str):
     db = SessionLocal()
